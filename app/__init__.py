1c733710daad78c4eaa3980cf585ce4bcfb84fa3
Iliass Raihani <i.raihani@outlook.com>
2021-07-29 21:04:41 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -2,27 +2,28 @@ from configs import database
 from strategies import telecontact, charika
 from modules.database import Database
 from app.modules.vortex import Vortex
+from app.modules.logger import Logger
 from random import randint
 import time
 
 
 def main():
-    db = Database(database.DATABASE)
+    # db = Database(database.DATABASE)
     vrt = Vortex(telecontact)
+    log = Logger()
 
     request = vrt.build_request(1)
     vrt.suck_page(request)
-    vrt.map_links()
+    vrt.set_links()
     for link in vrt.links:
-        request = telecontact.URL + link
+        request = vrt.strategy.URL + link
         vrt.suck_page(request)
         data = vrt.map_by_strategy()
-        print(data)
-        db.store_data(data, telecontact.NAME)
+        # db.store_data(data, vrt.strategy.NAME)
         time.sleep(randint(1, 3))
 
-    db.fetch(""" SELECT * FROM companies """)
-    db.disconnect()
+    # db.fetch(""" SELECT * FROM companies """)
+    # db.disconnect()
 
 
 if __name__ == "__main__":
