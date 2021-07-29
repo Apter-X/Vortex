a6c51107e955a01d50142c1f75b5c1d1a24875fe
Iliass Raihani <i.raihani@outlook.com>
2021-07-29 19:02:57 +0100

    complete logger class

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -8,14 +8,27 @@ class Logger:
     def __init__(self):
         conf_path = os.path.dirname(__file__)
         conf_path = conf_path[:-7]
-        conf_path = conf_path + "configs\\log.yml"
+        conf_path = conf_path + "configs\\logconfig.yml"
 
         with open(conf_path, 'r') as stream:
             try:
-                self.config = yaml.safe_load(stream)
+                config = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
+        logging.config.dictConfig(config)
+        self.log = logging.getLogger(__name__)
 
-    def initialize(self):
-        logging.config.dictConfig(self.config)
-        logging.info('All good!')
+    def debug(self, msg):
+        self.log.debug(msg)
+
+    def info(self, msg):
+        self.log.info(msg)
+
+    def warning(self, msg):
+        self.log.warning(msg)
+
+    def error(self, msg):
+        self.log.error(msg)
+
+    def critical(self, msg):
+        self.log.critical(msg)
