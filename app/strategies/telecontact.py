e683c40303d612d03261417a1781bf89c57f5161
Iliass Raihani <i.raihani@outlook.com>
2021-07-29 09:30:36 +0100

    create telecontact strategy

--- app/strategies/telecontact.py
+++ app/strategies/telecontact.py
@@ -19,9 +19,15 @@ LINK = {
 }
 STRATEGY = {
     "name": ["find_child", ["a", {"itemtype": "https://schema.org/WebPage"}], ["span", {"itemprop": "name"}]],
-    "address": ["find", ["div", {"class": "results-adress"}]],
-    "description": ["find", ["div", {"class": "description"}]],
-    "prestation": ["find_all", ["a", {"class": "rubrique-client"}]],
-    "phone_number": ["find", ["div", {"class": "tel"}]],
-    "website": ["get_href", ["div", {"itemprop": "url"}]]
+    "legal_form": ["find", ["td", {"class", "forme_td"}]],
+    "capital": ["find", ["td", {"class": "capi_td"}]],
+    "effective": ["find", ["td", {"class": "effic_td"}]],
+    "year": ["find", ["td", {"class", "annee_creation_td"}]],
+    "type": ["find", ["td", {"class", "type_eta_td"}]],
+    "website": ["get_href", ["a", {"class", "btn-results-products"}]],
+    "telephone": ["find_all", ["div", {"class", "letel"}]],  # Need to be clean
+    # "address": ["find_all", ["div", {"class": "col-xs-12"}]],  # Need to be clean
+    "about": ["find", ["p", {"itemprop": "description"}]],
+    "products_services": ["find_all", ["a", {"class": "rubrique-client"}]],
+    "activities": ["find_all", ["a", {"style": "margin:2px;font-size: 17px;"}]]
 }
