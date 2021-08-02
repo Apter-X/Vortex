0b057de475869419dab4419478438b7579d03039
Iliass Raihani <i.raihani@outlook.com>
2021-08-02 18:18:46 +0100

    create regex builder method

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -1,6 +1,55 @@
 import logging
 import logging.config
 import os
+import re
+
+import yaml
+
+# ([0-9]{4})-([0-1][0-9])-([0-3][0-9]).([0-1][0-9]|[2][0-3]):
+# ([0-5][0-9]):([0-5][0-9]),([0-9]{3}).-.(urllib3.connectionpool).-.(DEBUG)
+def build_regex(name, level, y=None, m=None, d=None, h=None, mi=None, s=None, ms=None):
+    expression = "("
+    if y:
+        expression += y
+    else:
+        expression += "[0-9]{4}"
+    expression += ")-("
+    if m:
+        expression += m
+    else:
+        expression += "[0-1][0-9]"
+    expression += ")-("
+    if d:
+        expression += d
+    else:
+        expression += "[0-3][0-9]"
+    expression += ").("
+    if h:
+        expression += h
+    else:
+        expression += "[0-1][0-9]|[2][0-3]"
+    expression += "):("
+    if mi:
+        expression += mi
+    else:
+        expression += "[0-5][0-9]"
+    expression += "):("
+    if s:
+        expression += s
+    else:
+        expression += "[0-5][0-9]"
+    expression += "),("
+    if ms:
+        expression += ms
+    else:
+        expression += "[0-9]{3}"
+    expression += ").-.("
+    expression += name
+    expression += ").-.("
+    expression += level
+    expression += ")"
+
+    return expression
 
 
 class Logger:
