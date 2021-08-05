f855f3ea6cf3d1fae401efe848385cca09f87cb7
Iliass Raihani <i.raihani@outlook.com>
2021-08-05 18:56:50 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from configs import database
 from strategies import telecontact, charika
 from modules.database import Database
 from app.modules.vortex import Vortex
+from app.modules.system import System
 from app.modules.logger import Logger, build_regex
 from random import randint
 import time
@@ -12,9 +13,14 @@ def main():
     vrt = Vortex(telecontact)
     log = Logger()
 
-    expression = build_regex("urllib3.connectionpool", "DEBUG")
-    print(expression)
-    log.find_all(expression)
+    # sys = System()
+    # path_desktop = sys.root[:-19]
+    # logstash_path = path_desktop + "ElasticStack\\logstash-7.13.4\\bin\\logstash.bat"
+    # sys.execute_bat(logstash_path)
+
+    # expression = build_regex("urllib3.connectionpool", "DEBUG")
+    # print(expression)
+    # log.find_all(expression)
 
     # request = vrt.build_request(1)
     # vrt.suck_page(request)
@@ -26,7 +32,7 @@ def main():
         # db.store_data(data, vrt.strategy.NAME)
         # time.sleep(randint(1, 3))
 
-    # db.fetch(""" SELECT * FROM companies """)
+    # db.fetch(""" SELECT * FROM businesses """)
     # db.disconnect()
 
 
