30e4d12f580ae3597fc8737f9b4d1e1a973f0427
Iliass Raihani <i.raihani@outlook.com>
2021-08-09 12:51:26 +0100

    Update logger.py

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -3,59 +3,38 @@ import logging.config
 from app.modules.system import System
 import re
 
-# ([0-9]{4})-([0-1][0-9])-([0-3][0-9]).([0-1][0-9]|[2][0-3]):
-# ([0-5][0-9]):([0-5][0-9]),([0-9]{3}).-.(urllib3.connectionpool).-.(DEBUG)
-def build_regex(name, level, y=None, m=None, d=None, h=None, mi=None, s=None, ms=None):
-    expression = "("
-    if y:
-        expression += y
-    else:
-        expression += "[0-9]{4}"
-    expression += ")-("
-    if m:
-        expression += m
-    else:
-        expression += "[0-1][0-9]"
-    expression += ")-("
-    if d:
-        expression += d
-    else:
-        expression += "[0-3][0-9]"
-    expression += ").("
-    if h:
-        expression += h
-    else:
-        expression += "[0-1][0-9]|[2][0-3]"
-    expression += "):("
-    if mi:
-        expression += mi
-    else:
-        expression += "[0-5][0-9]"
-    expression += "):("
-    if s:
-        expression += s
-    else:
-        expression += "[0-5][0-9]"
-    expression += "),("
-    if ms:
-        expression += ms
-    else:
-        expression += "[0-9]{3}"
-    expression += ").-.("
-    expression += name
-    expression += ").-.("
-    expression += level
-    expression += ")"
 
+def build_regex(name, level='DEBUG|INFO|WARNING|ERROR|CRITICAL', y='[0-9]{4}', m='[0-1][0-9]', d='[0-3][0-9]',
+                h='[0-1][0-9]|[2][0-3]', mi='[0-5][0-9]', s='[0-5][0-9]', ms='[0-9]{3}'):
+    expression = '('
+    expression += y
+    expression += ')-('
+    expression += m
+    expression += ')-('
+    expression += d
+    expression += ').('
+    expression += h
+    expression += '):('
+    expression += mi
+    expression += '):('
+    expression += s
+    expression += '),('
+    expression += ms
+    expression += ').-.('
+    expression += name
+    expression += ').-.('
+    expression += level
+    expression += ')'
     return expression
 
 
 class Logger(System):
     def __init__(self):
         super().__init__()
-        config = self.read_yml("configs\\logconfig.yml", True)
+        config = self.read_yml('configs\\logconfig.yml', True)
         logging.config.dictConfig(config)
         self.log = logging.getLogger(__name__)
+        self.results = []
 
     def debug(self, msg):
         self.log.debug(msg)
@@ -73,9 +52,10 @@ class Logger(System):
         self.log.critical(msg)
 
     def find_all(self, regular_expression):
-        lines = self.get_file_lines("loginfo.log", True)
+        lines = self.get_file_lines('loginfo.log', True)
         q = re.compile(regular_expression)
         for line in lines:
             key_val = q.findall(line)
             if key_val:
+                self.results.append(line)
                 print(line)
