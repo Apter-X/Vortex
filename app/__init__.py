96c88cb606e1363dddd2e9df984608fa9ed44d82
Iliass Raihani <i.raihani@outlook.com>
2021-08-09 12:52:11 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,16 @@
-from configs import database
 from strategies import telecontact, charika
-from modules.database import Database
+from app.modules.factory import Factory
 from app.modules.vortex import Vortex
-from app.modules.system import System
-from app.modules.logger import Logger, build_regex
-from random import randint
-import time
+from app.modules.logger import build_regex
 
 
 def main():
-    # db = Database(database.DATABASE)
-    vrt = Vortex(telecontact)
-    log = Logger()
+    engine = Vortex(telecontact)
+    factory = Factory('schema', engine)
+    # factory.extract(1, 2)
 
-    # sys = System()
-    # path_desktop = sys.root[:-19]
-    # logstash_path = path_desktop + "ElasticStack\\logstash-7.13.4\\bin\\logstash.bat"
-    # sys.execute_bat(logstash_path)
-
-    # expression = build_regex("urllib3.connectionpool", "DEBUG")
-    # print(expression)
-    # log.find_all(expression)
-
-    # request = vrt.build_request(1)
-    # vrt.suck_page(request)
-    # vrt.set_links()
-    # for link in vrt.links:
-    #     request = vrt.strategy.URL + link
-    #     vrt.suck_page(request)
-    #     data = vrt.map_by_strategy()
-        # db.store_data(data, vrt.strategy.NAME)
-        # time.sleep(randint(1, 3))
-
-    # db.fetch(""" SELECT * FROM businesses """)
-    # db.disconnect()
+    expression = build_regex("app.modules.logger")
+    factory.logger.find_all(expression)
 
 
 if __name__ == "__main__":
