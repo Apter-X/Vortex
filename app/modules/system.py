66a741e5c75b8b1fa7309c1f47bdb65ae766cabd
Iliass Raihani <i.raihani@outlook.com>
2021-08-18 13:30:48 +0100

    create clear console method

--- app/modules/system.py
+++ app/modules/system.py
@@ -1,6 +1,17 @@
 import os
 import subprocess
 import yaml
+import pyautogui
+
+
+def clear():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
 
 
 class System:
