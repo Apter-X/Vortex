126d64dc62ad8560c68d30533017e42bbf9de207
Iliass Raihani <43287534+Apter-X@users.noreply.github.com>
2023-03-09 18:34:09 +0100

    fix modules importation

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,12 @@
 import os
 from pathlib import Path
 
-from modules import System, Logger, Database, Factory
+from modules.system import System, BColors, clear, argv
+from modules.logger import Logger
+from modules.database import Database
+from modules.mapper import Mapper
+from modules.vortex import Vortex
+from modules.factory import Factory
 
 from dotenv import load_dotenv
 load_dotenv()
