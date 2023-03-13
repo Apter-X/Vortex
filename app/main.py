c807471b2db26375c21f199ba9d502dd9929f53c
Iliass Raihani <43287534+Apter-X@users.noreply.github.com>
2023-03-13 16:00:56 +0100

    update main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ from settings import db, factory, Vortex, Mapper, argv
 
 
 def main():
-    target = str(argv[1])
-    current = int(argv[2])
-    end = int(argv[3])
+    if len(argv) > 1:
+        target = str(argv[1])
+        current = int(argv[2])
+        end = int(argv[3])
+    else:
+        target = "avito"
+        current = 1
+        end = 30
 
     schema_record = db.fetch_assoc(f"""SELECT * FROM schemas WHERE schemas_name = '{target.title()}'""")
 
@@ -15,14 +20,16 @@ def main():
         "queries": schema_record.schemas_query_params,
         "headers": schema_record.schemas_request_headers,
         "link": schema_record.schemas_link,
-        "map": schema_record.schemas_map
+        "map": schema_record.schemas_map,
+        "base": schema_record.schemas_base
     }
 
     mapper = Mapper(schema)
     engine = Vortex(mapper)
     factory.engine = engine
-    factory.start(current, end)
-    # factory.try_target("https://www.avito.ma/fr/yacoub_el_mansour/voitures_d'occasion/Volkswagen_touareg_Diesel_2016_52023842.htm")
+    factory.start(current, end, schema['base'])
+    # factory.try_set_links("https://www.moteur.ma/fr/voiture/achat-voiture-occasion/")
+    # factory.try_target("https://www.moteur.ma/fr/voiture/achat-voiture-occasion/detail-annonce/417911/nissan-micra-.html")
 
 
 if __name__ == '__main__':
